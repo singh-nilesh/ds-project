ea86e254d1205b4f909aa05d582719174b00eb45
singh-nilesh <nileshsingh2021fybsc@gmail.com>
2025-04-29 18:09:31 +0530

    seprate extra data cols

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,6 +12,7 @@ class DataIngestionConfig:
     train_data_path: str=os.path.join('artifact', 'train.csv')
     test_data_path: str=os.path.join('artifact', 'test.csv')
     raw_data_path: str=os.path.join('artifact', 'raw.csv')
+    extra_data_path: str=os.path.join('artifact', 'extra_cols.csv')
     
 
 class DataIngestion:
@@ -33,6 +34,12 @@ class DataIngestion:
                 exist_ok=True
                 )
             
+            # Segregating String cols
+            logging.info('Dropping Extra columns') 
+            str_cols = ['Preferred-job', 'Preferred-industry', 'Redo-subjects', 'Current-skills', 'Job-portals', 'Missing-subjects', 'Certification-domain', 'Domain', 'Location']
+            extra_cols = df[str_cols].copy()
+            df = df.drop(str_cols, axis=1)
+            
             # split the dataset
             logging.info("Train test split initiated")
             train_set,test_set = train_test_split(df, test_size=0.2, random_state=42)
@@ -41,6 +48,7 @@ class DataIngestion:
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True) #raw
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True) #train
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True) #test
+            extra_cols.to_csv(self.ingestion_config.extra_data_path, index=False, header=True) #extra
             
             logging.info('Ingestion completed')
             
